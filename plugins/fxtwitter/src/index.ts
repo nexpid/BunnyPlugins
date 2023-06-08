58f7bf4b41abd154327d438d42353ea33dca15b8
Gabe616 <poggerschampersqwq@gmail.com>
2023-06-08 14:58:37 +0200

    feat: finish fxtwitter plugin (discontinued lol)

---
+++ plugins/fxtwitter/src/index.ts
@@ -0,0 +1,27 @@
+import { storage } from "@vendetta/plugin";
+import { findByProps } from "@vendetta/metro";
+import Settings, { hosts } from "./settings";
+import { patcher } from "@vendetta";
+
+const sendMessage = findByProps("sendMessage", "receiveMessage");
+
+const unpatch = patcher.before("sendMessage", sendMessage, (args) => {
+  const message = args[1];
+  const content = message?.content;
+  if (!message || !content || typeof message !== "object") return;
+
+  const host = hosts.find((x) => storage.fxtwthost === x.id);
+  if (!host) return;
+
+  message.content = content.replace(
+    /https?:\/\/(www\.)?twitter\.com/g,
+    host.url
+  );
+
+  args[1] = message;
+});
+
+export default {
+  onUnload: () => unpatch,
+  settings: Settings,
+};
