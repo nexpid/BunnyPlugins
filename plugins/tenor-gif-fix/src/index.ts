bb350959e13041abbe345e908c2ba8b8c48657d0
Gabe616 <poggerschampersqwq@gmail.com>
2023-06-10 09:25:33 +0200

    feat: update tenor-gif-fix

--- plugins/tenor-gif-fix/src/index.ts
+++ plugins/tenor-gif-fix/src/index.ts
@@ -1,23 +1,61 @@
-import { patcher } from "@vendetta";
+import { before } from "@vendetta/patcher";
 import { findByProps } from "@vendetta/metro";
 
-const MediaManager = findByProps("downloadMediaAsset"); // thank you rosie
+const MediaManager = findByProps("downloadMediaAsset"); // thank you rosie for this snippet
+const ActionSheet = findByProps("openLazy", "hideActionSheet");
 
-const unpatch = patcher.before("downloadMediaAsset", MediaManager, (args) => {
-  const uri = args[0];
-  if (!uri || typeof uri !== "string") return;
-
-  const path = uri.split("/");
+const parseURL = (url: string): string | undefined => {
+  const path = url.split("/");
   const tenorIndex = path.findIndex((x) => x.endsWith(".tenor.com"));
   if (tenorIndex === -1) return;
 
   const [host, id, file] = path.slice(tenorIndex, tenorIndex + 3);
   if (!host || !id || !file) return;
 
-  args[0] = `https://${host}/${id.slice(0, -2)}AC/${file.split(".")[0]}.gif`;
-  args[1] = 1;
-});
+  return `https://${host}/${id.slice(0, -2)}AC/${file.split(".")[0]}.gif`;
+};
+
+let patches = [];
 
 export default {
-  onUnload: () => unpatch,
+  onLoad: () => {
+    // keep this here just in case
+    patches.push(
+      before("downloadMediaAsset", MediaManager, (args) => {
+        const url = args[0];
+        if (!url || typeof url !== "string") return;
+
+        const parsed = parseURL(url);
+        if (parsed) {
+          args[0] = parsed;
+          args[1] = 1;
+        }
+      })
+    );
+
+    patches.push(
+      before("openLazy", ActionSheet, (ctx) => {
+        const [_, action, args] = ctx;
+
+        if (action !== "MediaShareActionSheet") return;
+
+        const data = args?.syncer?.sources?.[0];
+        if (!data) return;
+
+        const parsed = parseURL(data.uri);
+        if (parsed) {
+          data.uri = parsed;
+          data.sourceURI = parsed;
+          delete data.videoURI;
+          delete data.isGIFV;
+        }
+
+        args.syncer.sources[0] = data;
+      })
+    );
+  },
+  onUnload: () => {
+    for (const x of patches) x();
+    patches = [];
+  },
 };
