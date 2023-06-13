55efef3bc853c79ecc2e2089ee1eeb7be7f3af93
Gabe616 <poggerschampersqwq@gmail.com>
2023-06-13 17:35:29 +0200

    style: better code

--- plugins/fxtwitter/src/index.ts
+++ plugins/fxtwitter/src/index.ts
@@ -5,23 +5,26 @@ import { patcher } from "@vendetta";
 
 const sendMessage = findByProps("sendMessage", "receiveMessage");
 
-const unpatch = patcher.before("sendMessage", sendMessage, (args) => {
-  const message = args[1];
-  const content = message?.content;
-  if (!message || !content || typeof message !== "object") return;
-
-  const host = hosts.find((x) => storage.fxtwthost === x.id);
-  if (!host) return;
-
-  message.content = content.replace(
-    /https?:\/\/(www\.)?twitter\.com/g,
-    host.url
-  );
-
-  args[1] = message;
-});
+let unpatch;
 
 export default {
-  onUnload: () => unpatch,
+  onLoad: () => {
+    unpatch = patcher.before("sendMessage", sendMessage, (args) => {
+      const message = args[1];
+      const content = message?.content;
+      if (!message || !content || typeof message !== "object") return;
+
+      const host = hosts.find((x) => storage.fxtwthost === x.id);
+      if (!host) return;
+
+      message.content = content.replace(
+        /https?:\/\/(www\.)?twitter\.com/g,
+        host.url
+      );
+
+      args[1] = message;
+    });
+  },
+  onUnload: () => unpatch?.(),
   settings: Settings,
 };
