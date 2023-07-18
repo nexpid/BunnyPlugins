da29a6601f60ae4c86d7d6b8a4864eb11bb02259
Gabe616 <poggerschampersqwq@gmail.com>
2023-07-18 18:12:52 +0200

    feat(stuff): add CustomBadgeTag wowie

---
+++ stuff/CustomBadgeTag.tsx
@@ -0,0 +1,41 @@
+import { stylesheet } from "@vendetta/metro/common";
+import { semanticColors } from "@vendetta/ui";
+import { General } from "@vendetta/ui/components";
+import { SimpleText } from "./types";
+
+const { View } = General;
+const styles = stylesheet.createThemedStyleSheet({
+  main: {
+    marginRight: 16,
+    flexGrow: 0,
+  },
+  content: {
+    backgroundColor: semanticColors.REDESIGN_BUTTON_PRIMARY_BACKGROUND,
+    borderRadius: 16,
+    marginLeft: 8,
+    paddingHorizontal: 8,
+  },
+});
+
+export default function ({
+  text,
+  marginLeft,
+}: {
+  text: string;
+  marginLeft?: boolean;
+}) {
+  return (
+    <View
+      style={[
+        styles.main,
+        marginLeft ? { marginLeft: 16, marginRight: 0 } : {},
+      ]}
+    >
+      <View style={styles.content}>
+        <SimpleText variant="eyebrow" color="TEXT_NORMAL">
+          {text}
+        </SimpleText>
+      </View>
+    </View>
+  );
+}
