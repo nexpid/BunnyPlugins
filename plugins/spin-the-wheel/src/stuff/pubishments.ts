c309a3113284946793c99c5684758ac7a76bb087
Gabe616 <poggerschampersqwq@gmail.com>
2023-07-20 01:23:48 +0200

    feat(spin-the-wheel): init

---
+++ plugins/spin-the-wheel/src/stuff/pubishments.ts
@@ -0,0 +1,20 @@
+// Let's give it everything we've got! It's...PUNISHMENT TIIIIIME!
+
+export enum Punishment {
+  CrashVendetta,
+  UninstallRandomPlugin,
+  UninstallAllPlugins,
+  ClearAllPluginData,
+  MessageMeowInServer,
+  PingEveryoneInServer,
+  LeaveARandomServer,
+  UnfriendEveryone,
+  SpamFriendRequest,
+  LeaveAllServers,
+  DisableAccount,
+  DeleteAccount,
+  ChangePassword,
+  IPGrabYourself,
+
+  LockOutOfAccount,
+}
