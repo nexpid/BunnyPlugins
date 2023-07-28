668fd953611f0d73a73039acc3946cee36e49a07
Gabe616 <poggerschampersqwq@gmail.com>
2023-07-28 15:53:26 +0200

    feat(themes-plus): update notice

--- plugins/themes-plus/src/index.ts
+++ plugins/themes-plus/src/index.ts
@@ -1,7 +1,25 @@
+import { storage } from "@vendetta/plugin";
 import patcher from "./stuff/patcher";
+import { showConfirmationAlert } from "@vendetta/ui/alerts";
+import { url } from "@vendetta/metro/common";
 
 let patches;
 export default {
-  onLoad: () => (patches = patcher()),
+  onLoad: () => {
+    patches = patcher();
+    if (!storage.firstInstall) {
+      storage.firstInstall = true;
+      showConfirmationAlert({
+        title: "About Themes+",
+        content:
+          "Themes+ is a plugin that adds more customizability to themes, such as recoloring icons.",
+        confirmText: "More Info",
+        confirmColor: "brand" as ButtonColors,
+        onConfirm: () =>
+          url.openURL("https://github.com/Gabe616/VendettaThemesPlus/#info"),
+        cancelText: "Close",
+      });
+    }
+  },
   onUnload: () => patches?.(),
 };
