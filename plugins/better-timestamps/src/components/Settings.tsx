a7cff3176688440ce5d58d2c3cfa41e445a5ae9a
Gabe616 <poggerschampersqwq@gmail.com>
2023-07-28 22:01:29 +0200

    feat(better-timestamps): big ass update optimizations ofc date support relative timestamp support

--- plugins/better-timestamps/src/components/Settings.tsx
+++ plugins/better-timestamps/src/components/Settings.tsx
@@ -1,146 +1,123 @@
-import { React, constants } from "@vendetta/metro/common";
-import { stylesheet } from "@vendetta/metro/common";
+import { React } from "@vendetta/metro/common";
 import { useProxy } from "@vendetta/storage";
 import { getAssetIDByName } from "@vendetta/ui/assets";
 import { Forms, General } from "@vendetta/ui/components";
 import { vstorage } from "..";
-import { semanticColors } from "@vendetta/ui";
-import { showToast } from "@vendetta/ui/toasts";
-import { BetterTableRowGroup } from "../../../../stuff/types";
-import { findByProps } from "@vendetta/metro";
+import {
+  BetterTableRowGroup,
+  RichText,
+  SimpleText,
+} from "../../../../stuff/types";
+import { TimeExample } from "./examples/TimeExample";
+import { storage } from "@vendetta/plugin";
+import { findByProps, findByTypeName } from "@vendetta/metro";
+import { DateExample } from "./examples/DateExample";
 
-const { ScrollView, Text } = General;
+const { View, ScrollView } = General;
 const { FormSwitchRow, FormIcon } = Forms;
 
-export const { parseTimestamp } = findByProps(
-  "parseTimestamp",
-  "unparseTimestamp"
-);
-
-export function TimeExample({ style }: { style: any }): React.JSX.Element {
-  const [_, forceUpdate] = React.useReducer((x) => ~x, 0);
-
-  const nextSecond = new Date().setMilliseconds(1000);
-  setTimeout(() => forceUpdate(), nextSecond - Date.now());
-
-  const current = new Date();
-  const examples = [
-    [4],
-    [19, 20],
-    [4, 18, 2],
-    current.getHours() < 12 && [current.getHours()],
-    [current.getHours(), current.getMinutes()],
-    [current.getHours(), current.getMinutes(), current.getSeconds()],
-  ].filter((x) => !!x) as [number, number | undefined, number | undefined][];
-  const slay = [];
-
-  let i = -1;
-  for (const [hours, minutes, seconds] of examples) {
-    i++;
-    const date = new Date();
-    date.setHours(hours, minutes ?? 0, seconds ? seconds : 0);
-
-    let form = [
-      date.getHours().toString(),
-      minutes !== undefined && date.getMinutes().toString().padStart(2, "0"),
-      seconds !== undefined && date.getSeconds().toString().padStart(2, "0"),
-    ]
-      .filter((x) => !!x)
-      .join(":");
-    if (hours < 12) form += " AM";
-
-    const time = parseTimestamp(
-      Math.floor(date.getTime() / 1000).toString(),
-      vstorage.alwaysLong ? "T" : "t"
-    );
-
-    slay.push(
-      vstorage.reqBackticks ? <Text style={style.code}>{form}</Text> : form
-    );
-    slay.push(" — ");
-    slay.push(
-      <Text
-        style={style.timestamp}
-        onPress={() =>
-          showToast(time.full, getAssetIDByName("ic_information_24px"))
-        }
-      >
-        {time.formatted}
-      </Text>
-    );
-    if (i !== examples.length - 1) slay.push("\n");
-  }
-
-  return <>{...slay}</>;
-}
+const { BadgableTabBar } = findByProps("BadgableTabBar");
 
 export default () => {
-  vstorage.reqBackticks ??= true;
-  vstorage.alwaysLong ??= false;
-  useProxy(vstorage);
+  const [tab, setTab] = React.useState<"time" | "day">("time");
 
-  const style = stylesheet.createThemedStyleSheet({
-    mainText: {
-      fontFamily: constants.Fonts.DISPLAY_NORMAL,
-      includeFontPadding: false,
-      fontSize: 16,
-      color: semanticColors.TEXT_NORMAL,
-    },
-    boldText: {
-      fontFamily: constants.Fonts.DISPLAY_BOLD,
-      includeFontPadding: false,
-      color: semanticColors.TEXT_NORMAL,
-    },
-    code: {
-      fontFamily: constants.Fonts.CODE_SEMIBOLD,
-      includeFontPadding: false,
-      color: semanticColors.TEXT_NORMAL,
-      backgroundColor: semanticColors.BACKGROUND_SECONDARY,
-    },
-    timestamp: {
-      fontFamily: constants.Fonts.DISPLAY_NORMAL,
-      includeFontPadding: false,
-      color: semanticColors.TEXT_NORMAL,
-      backgroundColor: semanticColors.BACKGROUND_MODIFIER_ACCENT,
-    },
-  });
+  vstorage.time ??= {
+    acceptRelative: false,
+    requireBackticks: storage.reqBackticks ?? true,
+  };
+  vstorage.day ??= {
+    acceptRelative: false,
+    requireBackticks: true,
+    american: false,
+  };
+  if (storage.alwaysLong !== undefined) delete storage.alwaysLong;
+  if (storage.reqBackticks !== undefined) delete storage.reqBackticks;
+  useProxy(vstorage);
 
   return (
     <ScrollView>
+      <View style={{ marginHorizontal: 16, marginTop: 16 }}>
+        <BadgableTabBar
+          activeTab={tab}
+          onTabSelected={setTab}
+          tabs={[
+            { id: "day", title: "Date" },
+            { id: "time", title: "Time" },
+          ]}
+        />
+      </View>
       <BetterTableRowGroup
         title="Info"
         icon={getAssetIDByName("ic_info_24px")}
         padding={true}
       >
-        <Text style={style.mainText}>
-          Send a message with a time code (in
-          {<Text style={style.boldText}> HH:MM</Text>} or
-          {<Text style={style.boldText}> HH:MM:SS</Text>} format) and
-          automatically turn it into a timestamp{"\n\n"}
-          <TimeExample style={style} />
-        </Text>
+        <SimpleText variant="text-md/semibold" color="TEXT_NORMAL">
+          {tab === "time" ? (
+            <>
+              Send a message with a time code (in{" "}
+              <RichText.Bold>HH:MM</RichText.Bold>
+              {vstorage.time.acceptRelative ? ", " : " or "}
+              <RichText.Bold>HH:MM:SS</RichText.Bold>
+              {vstorage.time.acceptRelative && <> or relative</>} format) and
+              automatically turn it into a timestamp{"\n\n"}
+              <TimeExample />
+            </>
+          ) : (
+            <>
+              Send a message with a date (in{" "}
+              <RichText.Bold>
+                {vstorage.day.american ? "MM/DD" : "DD/MM"}/YY(YY)
+              </RichText.Bold>
+              {vstorage.day.acceptRelative ? ", " : " or "}
+              <RichText.Bold>
+                {vstorage.day.american ? "MM/DD" : "DD/MM"}/MM
+              </RichText.Bold>
+              {vstorage.day.acceptRelative && <> or relative</>} format) and
+              automatically turn it into a timestamp{"\n\n"}
+              <DateExample />
+            </>
+          )}
+        </SimpleText>
       </BetterTableRowGroup>
       <BetterTableRowGroup title="Settings" icon={getAssetIDByName("settings")}>
         <FormSwitchRow
-          label="Require Backticks"
-          subLabel="Require time to be surrounded by backticks"
+          key={`${tab}_acceptRelative`}
+          label={`Allow Relative ${tab === "time" ? "Time" : "Date"}`}
+          subLabel={
+            tab === "time"
+              ? "Formats relative time (in 10 seconds, 5 minutes ago)"
+              : "Formats relative date (in 2 days, 6 weeks ago)"
+          }
           leading={<FormIcon source={getAssetIDByName("ic_message_edit")} />}
-          onValueChange={() => (vstorage.reqBackticks = !vstorage.reqBackticks)}
-          value={vstorage.reqBackticks}
+          onValueChange={() =>
+            (vstorage[tab].acceptRelative = !vstorage[tab].acceptRelative)
+          }
+          value={vstorage[tab].acceptRelative}
         />
         <FormSwitchRow
-          label="Always Long Time"
-          subLabel={...[
-            "Always display ",
-            <Text style={style.boldText}>HH:MM:SS</Text>,
-            " instead of ",
-            <Text style={style.boldText}>HH:MM</Text>,
-          ]}
+          key={`${tab}_requireBackticks`}
+          label="Require Backticks"
+          subLabel={`Require ${tab} to be surrounded by backticks (\`)`}
           leading={<FormIcon source={getAssetIDByName("ic_message_edit")} />}
-          onValueChange={() => (vstorage.alwaysLong = !vstorage.alwaysLong)}
-          value={vstorage.alwaysLong}
+          onValueChange={() =>
+            (vstorage[tab].requireBackticks = !vstorage[tab].requireBackticks)
+          }
+          value={vstorage[tab].requireBackticks}
         />
+        {tab === "day" && (
+          <FormSwitchRow
+            key={`${tab}_american`}
+            label="'Merican Dates"
+            subLabel="Uses MM/DD instead of DD/MM"
+            leading={<FormIcon source={getAssetIDByName("ic_message_edit")} />}
+            onValueChange={() =>
+              (vstorage.day.american = !vstorage.day.american)
+            }
+            value={vstorage.day.american}
+          />
+        )}
       </BetterTableRowGroup>
+      <View style={{ marginBottom: 16 }} />
     </ScrollView>
   );
 };
--- plugins/better-timestamps/src/index.ts
+++ plugins/better-timestamps/src/index.ts
@@ -1,75 +1,22 @@
-import { findByProps } from "@vendetta/metro";
 import settings from "./components/Settings";
-import { before } from "@vendetta/patcher";
 import { storage } from "@vendetta/plugin";
-
-const MessageSender = findByProps("sendMessage", "receiveMessage");
+import patcher from "./stuff/patcher";
 
 export const vstorage: {
-  reqBackticks?: boolean;
-  reqMinutes?: boolean;
-  alwaysLong?: boolean;
+  time?: {
+    acceptRelative: boolean;
+    requireBackticks: boolean;
+  };
+  day?: {
+    acceptRelative: boolean;
+    requireBackticks: boolean;
+    american: boolean;
+  };
 } = storage;
-
-const isWhitespace = (x: string) => /^\s$/.test(x);
 
 let unpatch;
 export default {
-  onLoad: () => {
-    unpatch = before("sendMessage", MessageSender, (args) => {
-      const message = args[1];
-      let content = message?.content as string;
-      if (typeof content !== "string" || typeof message !== "object") return;
-
-      const j = vstorage.reqBackticks ? ["`", "`"] : ["(?:\\s|^)", "(?:\\s|$)"];
-
-      const reg = {
-        otN: "[0-9]{1,2}",
-        tN: `[0-9]{2}`,
-        abrv: "(?: ?(AM|PM))",
-      };
-      const regexes = [
-        `(${reg.otN}:${reg.tN}:${reg.tN})${reg.abrv}?`,
-        `(${reg.otN}:${reg.tN})${reg.abrv}?`,
-        `(${reg.otN})${reg.abrv}`,
-      ].map((x) => new RegExp(`${j[0]}${x}${j[1]}`, "gi"));
-
-      for (const reg of regexes) {
-        content = content.replace(reg, (str, time: string, abrv?: string) => {
-          let [hours, minutes, seconds] = time
-            .split(":")
-            .map((x) => parseInt(x)) as [
-            number,
-            number | undefined,
-            number | undefined
-          ];
-
-          if (hours < 0 || hours > 24) return str;
-          if (typeof minutes === "number" && (minutes < 0 || minutes > 60))
-            return str;
-          if (typeof seconds === "number" && (seconds < 0 || seconds > 60))
-            return str;
-
-          // why tf does US time work this way
-          if (abrv) {
-            if (abrv.toLowerCase() === "pm" && hours !== 12) hours += 12;
-            else if (abrv.toLowerCase() === "am" && hours === 12) hours = 0;
-          }
-
-          const date = new Date();
-          date.setHours(hours, minutes ?? 0, seconds ?? 0, 0);
-
-          return `${isWhitespace(str[0]) ? str[0] : ""}<t:${Math.floor(
-            date.getTime() / 1000
-          )}:${vstorage.alwaysLong ? "T" : "t"}>${
-            isWhitespace(str[str.length - 1]) ? str[str.length - 1] : ""
-          }`;
-        });
-      }
-
-      message.content = content;
-    });
-  },
+  onLoad: () => (unpatch = patcher()),
   onUnload: () => unpatch?.(),
   settings,
 };
