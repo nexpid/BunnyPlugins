1ffcb48f70ba586cccb23ee7bb40e1e99f6cdb98
Gabe616 <poggerschampersqwq@gmail.com>
2023-08-05 17:18:59 +0200

    fix(themes-plus): fix reloadUI

--- plugins/themes-plus/src/stuff/util.ts
+++ plugins/themes-plus/src/stuff/util.ts
@@ -1,17 +1,34 @@
-import { ReactNative as RN } from "@vendetta/metro/common";
+import { findByStoreName } from "@vendetta/metro";
+import { FluxDispatcher, ReactNative as RN } from "@vendetta/metro/common";
+
+const SelectivelySyncedUserSettingsStore = findByStoreName(
+  "SelectivelySyncedUserSettingsStore"
+);
 
 export function addToStyle(x: any, y: any) {
   x.style = Object.assign(RN.StyleSheet.flatten(x.style) ?? {}, y);
 }
 
 export function reloadUI() {
-  // const og = UnsyncedUserSettingsStore.useAMOLEDTheme;
-  // FluxDispatcher.dispatch({
-  //   type: "UNSYNCED_USER_SETTINGS_UPDATE",
-  //   settings: { useAMOLEDTheme: [0, 1, 2].find((x) => x !== og) },
-  // });
-  // FluxDispatcher.dispatch({
-  //   type: "UNSYNCED_USER_SETTINGS_UPDATE",
-  //   settings: { useAMOLEDTheme: og },
-  // });
+  const theme =
+    SelectivelySyncedUserSettingsStore.getAppearanceSettings().theme.slice();
+  const callback = () => {
+    SelectivelySyncedUserSettingsStore.removeChangeListener(callback);
+    FluxDispatcher.dispatch({
+      type: "SELECTIVELY_SYNCED_USER_SETTINGS_UPDATE",
+      changes: {
+        appearance: { settings: { theme } },
+      },
+    });
+  };
+  SelectivelySyncedUserSettingsStore.addChangeListener(callback);
+
+  FluxDispatcher.dispatch({
+    type: "SELECTIVELY_SYNCED_USER_SETTINGS_UPDATE",
+    changes: {
+      appearance: {
+        settings: { theme: theme === "light" ? "dark" : "light" },
+      },
+    },
+  });
 }
