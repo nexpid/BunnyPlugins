3e7b789d5df8b8d464e70379bc19ae60974b0d12
Gabe616 <poggerschampersqwq@gmail.com>
2023-08-14 12:45:31 +0200

    fix(nexxutils): fix reloadUI

--- plugins/nexxutils/src/modules/index.tsx
+++ plugins/nexxutils/src/modules/index.tsx
@@ -3,10 +3,12 @@ import Freemix from "./Freemix";
 import NoInviteToServers from "./NoInviteToServers";
 import SpotifyListenAlong from "./SpotifyListenAlong";
 import TenorGifFix from "./TenorGifFix";
+import Test from "./Test";
 
 export default [
   Freemix,
   TenorGifFix,
   SpotifyListenAlong,
   NoInviteToServers,
+  Test,
 ] as Module[];
