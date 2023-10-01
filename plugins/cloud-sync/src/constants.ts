5726a9fef9d2bff213db63b197b6dec620ea7ce2
Gabe616 <poggerschampersqwq@gmail.com>
2023-10-01 11:09:56 +0200

    feat(cloud-sync): fix API for oauth2

--- plugins/cloud-sync/src/constants.ts
+++ plugins/cloud-sync/src/constants.ts
@@ -1,15 +1,25 @@
 import { vstorage } from ".";
 
-const api = "https://cloudsync.nexpid.xyz/";
+const root = "https://cloudsync.nexpid.xyz/";
 export default {
   get api() {
-    if (vstorage.host)
-      return !vstorage.host.endsWith("/") ? `${vstorage.host}/` : vstorage.host;
-    else return api;
+    return vstorage.host
+      ? !vstorage.host.endsWith("/")
+        ? `${vstorage.host}/`
+        : vstorage.host
+      : root;
   },
   raw: "https://raw.githubusercontent.com/nexpid/VendettaPlugins/main/plugins/cloud-sync/",
   oauth2: {
     clientId: "1120793656878714913",
-    redirectURL: `${api}api/oauth2-response`,
+    get redirectURL() {
+      return `${
+        vstorage.host
+          ? !vstorage.host.endsWith("/")
+            ? `${vstorage.host}/`
+            : vstorage.host
+          : root
+      }api/oauth2-response`;
+    },
   },
 };
