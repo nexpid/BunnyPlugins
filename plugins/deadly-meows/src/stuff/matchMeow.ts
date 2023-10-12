308711bf5665d26ed2787dbbb04ae561b09f56ee
Gabe616 <poggerschampersqwq@gmail.com>
2023-10-12 22:34:05 +0200

    feat(plugins): archive some plugins

--- plugins/deadly-meows/src/stuff/matchMeow.ts
+++
@@ -1,4 +0,0 @@
-// mrrowwwww :3
-export const meowRegexes = [/\bmr*e?ow+\b/, /\bnya+\b/];
-
-export default (str: string) => meowRegexes.some((x) => x.test(str));
--- plugins/download-everything/src/stuff/cdn.ts
+++
@@ -1,7 +0,0 @@
-export const baseCdn = "https://cdn.discordapp.com/";
-export function cdnFormatAnimated(x: string) {
-  return `${x}.${animatedFileType(x)}?size=4096`;
-}
-export function animatedFileType(x: string) {
-  return x.startsWith("a_") ? "gif" : "png";
-}
