a48339351ff3c168617af423164c21a90b301bdc
nexpid <nexpid@nexpid.xyz>
2024-01-31 00:37:12 +0100

    feat(crowdin): attempt to fully add crowdin

---
+++ .github/scripts/crowdin.mjs
@@ -0,0 +1,22 @@
+import { readdir, readFile, writeFile } from "fs/promises";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
+const dir = dirname(fileURLToPath(import.meta.url));
+
+for (const lang of await readdir(".")) {
+  if (lang === "base") continue;
+
+  const data = {};
+  for (const file of await readdir(lang))
+    data[file.split(".")[0]] = JSON.parse(
+      await readFile(join(".", "lang", file), "utf8"),
+    );
+
+  await writeFile(
+    join(dir, "../../stuff/lang/values", lang + ".json"),
+    JSON.stringify(data, undefined, 4),
+  );
+  console.log(`Wrote ${lang}.json`);
+}
+console.log("Done");
