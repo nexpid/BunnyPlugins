12165312e64fe6afa6ec744115bb08500d10bee3
nexpid <nexpid@nexpid.xyz>
2024-01-30 22:11:10 +0100

    feat: update global.d.ts

--- global.d.ts
+++ global.d.ts
@@ -15,51 +15,10 @@ declare module "*.html" {
   export default content;
 }
 
-interface FileManager {
-  /**
-   * @param path **Full** path to file
-   */
-  fileExists: (path: string) => Promise<boolean>;
-  /**
-   * Allowed URI schemes on Android: `file://`, `content://` ([See here](https://developer.android.com/reference/android/content/ContentResolver#accepts-the-following-uri-schemes:_3))
-   */
-  getSize: (uri: string) => Promise<boolean>;
-  /**
-   * @param path **Full** path to file
-   * @param encoding Set to `base64` in order to encode response
-   */
-  readFile(path: string, encoding: "base64" | "utf8"): Promise<string>;
-  saveFileToGallery?(
-    uri: string,
-    fileName: string,
-    fileType: "PNG" | "JPEG",
-  ): Promise<string>;
-  /**
-   * Beware! This function has differing functionality on iOS and Android.
-   * @param storageDir Either `cache` or `documents`.
-   * @param path Path in `storageDir`, parents are recursively created.
-   * @param data The data to write to the file
-   * @param encoding Set to `base64` if `data` is base64 encoded.
-   * @returns Promise that resolves to path of the file once it got written
-   */
-  writeFile(
-    storageDir: "cache" | "documents",
-    path: string,
-    data: string,
-    encoding: "base64" | "utf8",
-  ): Promise<string>;
-  getConstants: () => {
-    /**
-     * The path the `documents` storage dir (see {@link writeFile}) represents.
-     */
-    DocumentsDirPath: string;
-  };
-  /**
-   * Will apparently cease to exist some time in the future so please use {@link getConstants} instead.
-   * @deprecated
-   */
-  DocumentsDirPath: string;
-}
+declare const IS_DEV: boolean;
+
+// Buffer types coming soon:tm:
+declare const Buffer: any;
 
 interface Window {
   nx?: {
@@ -72,14 +31,17 @@ interface Window {
     p: {
       wipe: () => void;
       snipe: (
-        key: string,
+        prop: string,
         parent: any,
         callback?: (args: any[], ret: any) => any,
         oneTime?: boolean,
       ) => void;
+      props: {
+        collect: (key: string, prop: string, parent: any) => void;
+        redeem: (key: string) => object | undefined;
+      };
     };
   };
-  nativeModuleProxy: any;
 
   __vendetta_loader:
     | {
@@ -91,6 +53,5 @@ interface Window {
     | undefined;
   __vendetta_theme: Theme | undefined;
 
-  CSmigrationStage: number | undefined;
   TPfirstLoad: boolean | undefined;
 }
