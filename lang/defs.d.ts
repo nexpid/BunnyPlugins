dc762e9ef2885d7d35dcb75c008fa3cc31ce84c5
nexpid <nexpid@nexpid.xyz>
2024-02-03 19:12:16 +0100

    feat(lang): silly fixes

--- lang/defs.d.ts
+++ lang/defs.d.ts
@@ -2,39 +2,39 @@ export interface LangValues {
   cloud_sync: {
     values: typeof import("./values/base/cloud_sync.json");
     fillers: {
-      "combo.plugins.plural": ["$count"];
-      "combo.themes.plural": ["$count"];
-      "toast.backup_saved": ["$file"];
-      "toast.errored": ["$error"];
-      "sheet.import_data.unproxied_plugins": ["$count"];
-      "sheet.import_data.plugins": ["$count"];
-      "sheet.import_data.themes": ["$count"];
-      "log.import.start.combo": ["$plugins", "$themes"];
-      "log.import.plugin.success": ["$name"];
-      "log.import.plugin.fail": ["$name", "$error"];
-      "log.import.theme.success": ["$name"];
-      "log.import.theme.fail": ["$name", "$error"];
-      "log.import.total": ["$plugins", "$themes"];
-      "log.import.select_theme": ["$theme"];
-      "log.import.result.success": ["$plugins", "$themes"];
+      "combo.plugins.plural": ["count"];
+      "combo.themes.plural": ["count"];
+      "toast.backup_saved": ["file"];
+      "toast.errored": ["error"];
+      "sheet.import_data.unproxied_plugins": ["count"];
+      "sheet.import_data.plugins": ["count"];
+      "sheet.import_data.themes": ["count"];
+      "log.import.start.combo": ["plugins", "themes"];
+      "log.import.plugin.success": ["name"];
+      "log.import.plugin.fail": ["name", "error"];
+      "log.import.theme.success": ["name"];
+      "log.import.theme.fail": ["name", "error"];
+      "log.import.total": ["plugins", "themes"];
+      "log.import.select_theme": ["theme"];
+      "log.import.result.success": ["plugins", "themes"];
       "log.import.result.fail": [
-        "$plugins",
-        "$themes",
-        "$failed_plugins",
-        "$failed_themes",
+        "plugins",
+        "themes",
+        "failed_plugins",
+        "failed_themes",
       ];
     };
   };
   plugin_browser: {
     values: typeof import("./values/base/plugin_browser.json");
     fillers: {
-      "plugin.name.changes": ["$changes"];
-      "toast.plugin.update.success": ["$plugin"];
-      "toast.plugin.update.fail": ["$plugin"];
-      "toast.plugin.delete.success": ["$plugin"];
-      "toast.plugin.delete.fail": ["$plugin"];
-      "toast.plugin.install.success": ["$plugin"];
-      "toast.plugin.install.fail": ["$plugin"];
+      "plugin.name.changes": ["changes"];
+      "toast.plugin.update.success": ["plugin"];
+      "toast.plugin.update.fail": ["plugin"];
+      "toast.plugin.delete.success": ["plugin"];
+      "toast.plugin.delete.fail": ["plugin"];
+      "toast.plugin.install.success": ["plugin"];
+      "toast.plugin.install.fail": ["plugin"];
     };
   };
   twemoji_everywhere: {
