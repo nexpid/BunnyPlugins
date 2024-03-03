14f9e31e41479668e670a1e8bc169ac7a01a6152
nexpid <nexpid@nexpid.xyz>
2024-03-03 23:13:17 +0100

    feat(stuff): make RedesignSlider actually compat

--- stuff/components/compat/RedesignSlider.tsx
+++ stuff/components/compat/RedesignSlider.tsx
@@ -1,6 +1,7 @@
-import { ReactNative as RN } from "@vendetta/metro/common";
+import { React, ReactNative as RN, stylesheet } from "@vendetta/metro/common";
 
-import { Redesign } from "$/types";
+import { Reanimated } from "$/deps";
+import { Redesign, resolveCustomSemantic } from "$/types";
 
 import SliderIcon from "../SliderIcon";
 
@@ -17,39 +18,198 @@ export default function RedesignSlider({
   minimumValue: number;
   maximumValue: number;
 }) {
+  const [pressing, setPressing] = React.useState(false);
+  const theBar = React.useRef<import("react-native").View>(null);
+
+  const dotScale = Reanimated.useSharedValue(14);
+  const shadowClr = Reanimated.useSharedValue("#00000000");
+
+  const liveValue = React.useRef(value);
+  liveValue.current = value;
+
+  const start = (
+    <SliderIcon
+      side="start"
+      onPress={() => {
+        const val = Math.min(
+          Math.max(value - step, minimumValue),
+          maximumValue,
+        );
+        if (val !== value) onValueChange(val);
+      }}
+    />
+  );
+  const end = (
+    <SliderIcon
+      side="end"
+      onPress={() => {
+        const val = Math.min(
+          Math.max(value + step, minimumValue),
+          maximumValue,
+        );
+        if (val !== value) onValueChange(val);
+      }}
+    />
+  );
+
+  if ("Slider" in Redesign)
+    return (
+      <RN.View
+        style={{ marginHorizontal: 16, marginTop: 16, marginBottom: 12 }}
+      >
+        <Redesign.Slider
+          value={value}
+          step={step}
+          onValueChange={(val) => value !== val && onValueChange?.(val)}
+          minimumValue={minimumValue}
+          startIcon={start}
+          maximumValue={maximumValue}
+          endIcon={end}
+        />
+      </RN.View>
+    );
+
+  const styles = stylesheet.createThemedStyleSheet({
+    base: {
+      flexDirection: "row",
+      alignItems: "center",
+      height: 24,
+      marginHorizontal: 16,
+      marginTop: 16,
+      marginBottom: 12,
+    },
+    theBar: {
+      // TODO make this not forced
+      width: "70%",
+      marginHorizontal: 16,
+      height: "100%",
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    bar: {
+      width: "100%",
+      height: 2,
+      backgroundColor: resolveCustomSemantic("#ffffff0d", "#00000010"),
+    },
+    fullBar: {
+      height: "100%",
+      backgroundColor: "#5865f2",
+      flexDirection: "row",
+      justifyContent: "flex-end",
+      alignItems: "center",
+    },
+    dot: {
+      borderRadius: 11,
+      backgroundColor: "#c9cdfb",
+    },
+    dotShadow: {
+      width: 35,
+      height: 35,
+      transform: [{ translateX: 17.5 }],
+      borderRadius: 18,
+      alignItems: "center",
+      justifyContent: "center",
+    },
+  });
+
+  const pan = React.useRef(
+    RN.PanResponder.create({
+      onMoveShouldSetPanResponder() {
+        return true;
+      },
+      onMoveShouldSetPanResponderCapture() {
+        return true;
+      },
+      onStartShouldSetPanResponder() {
+        return true;
+      },
+      onStartShouldSetPanResponderCapture() {
+        return true;
+      },
+      onShouldBlockNativeResponder() {
+        return true;
+      },
+
+      onPanResponderMove(_, gesture) {
+        if (!theBar.current || gesture.moveX === 0) return;
+
+        theBar.current.measure((_, __, width, ___, x) => {
+          const endX = x + width;
+          const perc = (gesture.moveX - x) / (endX - x);
+
+          const val =
+            Math.round(
+              Math.min(
+                Math.max(perc * maximumValue, minimumValue),
+                maximumValue,
+              ) / step,
+            ) * step;
+
+          if (val !== liveValue.current && gesture.moveX !== 0) {
+            setPressing(true);
+            onValueChange(val);
+          }
+        });
+      },
+      onPanResponderEnd() {
+        setPressing(false);
+      },
+    }),
+  ).current;
+
+  React.useEffect(() => {
+    dotScale.value = Reanimated.withTiming(pressing ? 21 : 14, {
+      duration: 100,
+    });
+    shadowClr.value = Reanimated.withTiming(
+      pressing ? "#0000002a" : "#00000000",
+      { duration: 100 },
+    );
+  }, [pressing]);
+
   return (
-    <RN.View style={{ marginHorizontal: 16, marginTop: 16, marginBottom: 12 }}>
-      <Redesign.Slider
-        value={value}
-        step={step}
-        onValueChange={onValueChange}
-        minimumValue={minimumValue}
-        startIcon={
-          <SliderIcon
-            side="start"
-            onPress={() => {
-              const val = Math.min(
-                Math.max(value - step, minimumValue),
-                maximumValue,
-              );
-              if (val !== value) onValueChange(val);
-            }}
-          />
-        }
-        maximumValue={maximumValue}
-        endIcon={
-          <SliderIcon
-            side="end"
-            onPress={() => {
-              const val = Math.min(
-                Math.max(value + step, minimumValue),
-                maximumValue,
-              );
-              if (val !== value) onValueChange(val);
-            }}
-          />
-        }
-      />
+    <RN.View style={styles.base}>
+      <RN.View style={{ marginRight: 8 }}>{start}</RN.View>
+      <RN.View ref={theBar} style={styles.theBar} {...pan.panHandlers}>
+        <RN.View style={styles.bar}>
+          <Reanimated.default.View
+            style={[
+              styles.fullBar,
+              {
+                width:
+                  Math.min(
+                    Math.max(
+                      ((value - minimumValue) / (maximumValue - minimumValue)) *
+                        100,
+                      0,
+                    ),
+                    100,
+                  ) + "%",
+              },
+            ]}
+          >
+            <Reanimated.default.View
+              style={[
+                styles.dotShadow,
+                {
+                  backgroundColor: shadowClr,
+                },
+              ]}
+            >
+              <Reanimated.default.View
+                style={[
+                  styles.dot,
+                  {
+                    width: dotScale,
+                    height: dotScale,
+                  },
+                ]}
+              ></Reanimated.default.View>
+            </Reanimated.default.View>
+          </Reanimated.default.View>
+        </RN.View>
+      </RN.View>
+      <RN.View style={{ marginLeft: 8 }}>{end}</RN.View>
     </RN.View>
   );
 }
