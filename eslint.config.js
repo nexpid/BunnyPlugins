f47777f9981a0930b1682ef7238b0048c71dedc5
nexpid <60316309+nexpid@users.noreply.github.com>
2024-07-26 10:55:48 +0200

    feat: prefer prettier over stylistic

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,6 @@ import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 import simpleImportSort from "eslint-plugin-simple-import-sort";
 import unusedImports from "eslint-plugin-unused-imports";
-import stylistic from "@stylistic/eslint-plugin";
 import pathAlias from "eslint-plugin-path-alias";
 
 export default tseslint.config(
@@ -18,7 +17,6 @@ export default tseslint.config(
             },
         },
         plugins: {
-            "@stylistic": stylistic,
             "unused-imports": unusedImports,
             "simple-import-sort": simpleImportSort,
             "path-alias": pathAlias,
@@ -34,17 +32,26 @@ export default tseslint.config(
             "@typescript-eslint/no-non-null-asserted-nullish-coalescing":
                 "error",
             "@typescript-eslint/no-non-null-assertion": "error",
-            "no-useless-constructor": "off",
-            "@typescript-eslint/no-useless-constructor": "error",
             "@typescript-eslint/prefer-literal-enum-member": [
                 "error",
                 { allowBitwiseExpressions: true },
             ],
             "@typescript-eslint/unified-signatures": "error",
+            "no-useless-constructor": "off",
+            "@typescript-eslint/no-useless-constructor": "error",
 
             // https://github.com/Vendicated/Vencord/blob/main/.eslintrc.json
             eqeqeq: ["error", "always", { null: "ignore" }],
             yoda: "error",
+            "operator-assignment": "error",
+            "no-useless-computed-key": "error",
+            "no-unneeded-ternary": ["error", { defaultAssignment: false }],
+            "no-constant-condition": ["error", { checkLoops: "none" }],
+            "no-duplicate-imports": "error",
+            "dot-notation": "error",
+            "no-cond-assign": "error",
+            "prefer-const": "error",
+            "prefer-spread": "error",
             "prefer-destructuring": [
                 "error",
                 {
@@ -52,32 +59,6 @@ export default tseslint.config(
                     AssignmentExpression: { array: false, object: false },
                 },
             ],
-            "operator-assignment": "error",
-            "no-useless-computed-key": "error",
-            "no-unneeded-ternary": ["error", { defaultAssignment: false }],
-            "no-invalid-regexp": "error",
-            "no-constant-condition": ["error", { checkLoops: "none" }],
-            "no-duplicate-imports": "error",
-            "dot-notation": "error",
-            "no-useless-escape": "error",
-            "no-fallthrough": "error",
-            "for-direction": "error",
-            "no-async-promise-executor": "error",
-            "no-cond-assign": "error",
-            "no-dupe-else-if": "error",
-            "no-duplicate-case": "error",
-            "no-irregular-whitespace": "error",
-            "no-loss-of-precision": "error",
-            "no-misleading-character-class": "error",
-            "no-prototype-builtins": "error",
-            "no-regex-spaces": "error",
-            "no-shadow-restricted-names": "error",
-            "no-unexpected-multiline": "error",
-            "no-unsafe-optional-chaining": "error",
-            "no-useless-backreference": "error",
-            "use-isnan": "error",
-            "prefer-const": "error",
-            "prefer-spread": "error",
 
             // plugins
             "simple-import-sort/imports": "error",
