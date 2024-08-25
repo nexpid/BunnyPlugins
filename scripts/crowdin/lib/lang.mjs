e1a0d5a6c0429699dc3c48fbe7b4762548e1e09c
Jozef Steinhübl <xhyrom.hyro@gmail.com>
2024-08-25 17:40:34 +0200

    fix: hehe

--- scripts/crowdin/lib/lang.mjs
+++ scripts/crowdin/lib/lang.mjs
@@ -2,53 +2,50 @@ import { join } from "node:path";
 
 import { readdir, readFile, writeFile } from "fs/promises";
 
-import { listPlugins } from "../../build/modules/plugins.mjs";
+import { listPlugins } from "../../build/modules/plugins.ts";
 
 /**
  * @param {string} source
  */
 export async function writeNewLangFiles(source) {
-    /** @type {Record<string, Record<string, any>>} */
-    const data = {};
+  /** @type {Record<string, Record<string, any>>} */
+  const data = {};
 
-    // gather all translations from crowdin
-    for (const lang of await readdir(source)) {
-        if (lang === "base" || lang.endsWith(".json")) continue;
+  // gather all translations from crowdin
+  for (const lang of await readdir(source)) {
+    if (lang === "base" || lang.endsWith(".json")) continue;
 
-        for (const file of await readdir(join(source, lang))) {
-            const key = file.split(".")[0];
-            data[key] ??= {};
-            data[key][lang] = JSON.parse(
-                await readFile(join(source, lang, file), "utf8"),
-            );
-        }
+    for (const file of await readdir(join(source, lang))) {
+      const key = file.split(".")[0];
+      data[key] ??= {};
+      data[key][lang] = JSON.parse(
+        await readFile(join(source, lang, file), "utf8"),
+      );
     }
+  }
 
-    // go through all plugins
-    for (const { lang: plugin } of await listPlugins()) {
-        if (!plugin) continue;
+  // go through all plugins
+  for (const { lang: plugin } of await listPlugins()) {
+    if (!plugin) continue;
 
-        // if translations aren't on crowdin yet, make a placeholder
-        if (!data[plugin])
-            data[plugin] = {
-                en: JSON.parse(
-                    await readFile(
-                        join("lang/values/base", plugin + ".json"),
-                        "utf8",
-                    ),
-                ),
-            };
+    // if translations aren't on crowdin yet, make a placeholder
+    if (!data[plugin])
+      data[plugin] = {
+        en: JSON.parse(
+          await readFile(join("lang/values/base", plugin + ".json"), "utf8"),
+        ),
+      };
 
-        // minify but also not really
-        await writeFile(
-            join("lang/values", plugin + ".json"),
-            `{\n${Object.entries(data[plugin])
-                .sort((a, b) => a[0].localeCompare(b[0]))
-                .map(
-                    ([lang, entries]) =>
-                        `  ${JSON.stringify(lang)}: ${JSON.stringify(entries)}`,
-                )
-                .join(",\n")}\n}`,
-        );
-    }
+    // minify but also not really
+    await writeFile(
+      join("lang/values", plugin + ".json"),
+      `{\n${Object.entries(data[plugin])
+        .sort((a, b) => a[0].localeCompare(b[0]))
+        .map(
+          ([lang, entries]) =>
+            `  ${JSON.stringify(lang)}: ${JSON.stringify(entries)}`,
+        )
+        .join(",\n")}\n}`,
+    );
+  }
 }
