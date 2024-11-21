c42a1d58edd8c48d61e5123d2018722fc9473634
nexpid <60316309+nexpid@users.noreply.github.com>
2024-11-21 20:43:28 +0100

    feat(stuff): add ScaleRowButton

---
+++ src/stuff/components/ScaleRowButton.tsx
@@ -0,0 +1,19 @@
+import { PressableScale, RowButton } from "$/lib/redesign";
+import { RowButton as RowButtonType } from "$/lib/redesign/types";
+
+export default function ScaleRowButton({
+    onPress,
+    onPressIn,
+    onPressOut,
+    ...props
+}: Parameters<RowButtonType>[0]) {
+    return (
+        <PressableScale
+            onPress={onPress}
+            onPressIn={onPressIn}
+            onPressOut={onPressOut}
+            pointerEvents="box-only">
+            <RowButton {...props} />
+        </PressableScale>
+    );
+}
