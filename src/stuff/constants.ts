8096c1770a2897b45177927d4cdca7eec505bf96
nexpid <60316309+nexpid@users.noreply.github.com>
2024-11-25 19:53:10 +0100

    fix(src/stuff): use main branch

--- src/stuff/constants.ts
+++ src/stuff/constants.ts
@@ -1,7 +1,6 @@
 export default {
     github: {
-        // FIXME don't use custom branch
-        url: "https://github.com/nexpid/BunnyPlugins/tree/feat/improve-workspace",
-        raw: "https://raw.githubusercontent.com/nexpid/BunnyPlugins/feat/improve-workspace/",
+        url: "https://github.com/nexpid/BunnyPlugins/tree/main/",
+        raw: "https://raw.githubusercontent.com/nexpid/BunnyPlugins/main/",
     },
 };
