a7bd0f7feac9b68310555b8a78a05e05c9ee4bd9
nexpid <60316309+nexpid@users.noreply.github.com>
2024-12-10 20:08:04 +0100

    feat(src/stuff/types): add deepEquals func

--- src/stuff/types.tsx
+++ src/stuff/types.tsx
@@ -148,6 +148,20 @@ export function formatBytes(bytes: number) {
     return `${(bytes / Math.pow(1024, i)).toFixed(2)} ${sizes[i]}`;
 }
 
+export function deepEquals(x: any, y: any) {
+    if (x === y) return true;
+    else if (typeof x === "object" && typeof y === "object" && x && y) {
+        if (Object.keys(x).length !== Object.keys(y).length) return false;
+
+        for (const prop in x)
+            if (Object.prototype.hasOwnProperty.call(y, prop)) {
+                if (!deepEquals(x[prop], y[prop])) return false;
+            } else return false;
+
+        return true;
+    }
+}
+
 // ...
 
 export type TextStyleSheetCase = "normal" | "medium" | "semibold" | "bold";
